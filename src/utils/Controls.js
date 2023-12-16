fe57266ea39c4d2de81243e5f92d5d10c02993f7
James <jamessalafatinos@outlook.com>
2023-12-16 10:44:35 -0600

    added utils/modules

---
+++ src/utils/Controls.js
@@ -0,0 +1,34 @@
+import * as THREE from "/modules/three.module.js";
+import { OrbitControls } from "/modules/OrbitControls.js";
+import { OrbitControls_rs } from "/modules/OrbitControls_rs.js";
+import { NoClipControls } from "/utils/NoClipControls.js";
+
+
+
+function createControls(type, window, camera, document, renderer) {
+    let controls;
+  if (type == "noclip") {
+    camera.position.set(0, 1, 4);
+    controls = new NoClipControls(window, camera, document);
+    controls.flySpeed = 50
+
+  }
+  if (type == "orbit") {
+    controls = new OrbitControls(camera, renderer.domElement);
+    controls.enableDamping = true;
+  }
+  if (type == "orbit_rs") {
+    controls = new OrbitControls_rs(camera, renderer.domElement);
+    controls.enableDamping = true;
+  }
+  return controls;
+}
+
+function updateControls(controls) {
+  controls.update();
+}
+
+export {
+  createControls,
+  updateControls
+};
